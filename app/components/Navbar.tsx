f6f2599ba1d950eff51926876b718b5bd39ec342
Hrishikesh <hrishikeshrajan3@gmail.com>
2024-11-12 14:33:42 +0530

    add(navbar): sidebar to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 import Image from 'next/image'
 import { NavLinks } from './NavLinks'
 import AuthButtons from './AuthButton'
+import SideSheet from './SideSheet'
 function Navbar() {
     return (
         <header className='w-full  text-white px-4 py-4'>
             <nav className='max-w-7xl  mx-auto flex items-center justify-center lg:justify-between text-Skobeloff'  aria-label="Main navigation">
                 {/* Logo Section */}
+                <SideSheet />
                 <div className=' flex items-center bg-dark lg:gap-10 xl:gap-20'>
                     <Link href="/">
                         <Image
---
+++ app/components/SideSheet.tsx
@@ -0,0 +1,39 @@
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from 'next/image'
+
+function SideSheet() {
+  return (
+    <Sheet key={'left'}>
+   <SheetTrigger className='flex lg:hidden absolute top-5 left-5 p-2 focus:outline-none focus-visible:outline-none active:scale-90 transition-transform duration-200 ease-in-out focus:ring-0'>
+      <Image
+        src="/breadCrum.svg"
+        alt="Menu"
+        width={20}
+        height={20}
+        className='w-5 h-5 transition-all duration-300'
+        priority 
+      />
+    </SheetTrigger>
+      
+      <SheetContent side={'left'} className="focus:outline-none">
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default SideSheet
