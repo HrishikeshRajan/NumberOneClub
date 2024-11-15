371dce3446665c4d84326f3ff4fdcde3362d5a3f
Hrishikesh <hrishikeshrajan3@gmail.com>
2024-11-15 10:42:58 +0530

    refactor: search box button click animation 	- click animation updated to client side

--- app/components/SearchBox.tsx
+++ app/components/SearchBox.tsx
@@ -1,7 +1,10 @@
 
+
 import React from 'react'
 import Image from 'next/image'
 import Form from 'next/form';
+import CustomButton from './theme/CustomButton';
+import EnableClickAnimation from './animation/EnableClickAnimation';
 async function SearchBox({query}:{query?:string}) {
   return (
 <Form action='/' scroll={false} className='mt-8 w-full focus:outline-none'>
@@ -14,13 +17,17 @@ async function SearchBox({query}:{query?:string}) {
         placeholder="Search for Courses, Events, Instructors, Experts etc"
         className="flex-grow outline-none text-gray-600 placeholder-gray-400 text-sm md:text-base w-full focus:outline-none"
       />
-      <button 
-        type='submit' 
-        className="bg-Skobeloff gap-3 items-center flex text-white rounded-full px-5 py-2.5 ml-2 text-sm md:text-base font-medium focus:outline-none focus-visible:ring-2"
-      >
-        Search
-        <Image src="right-arrow.svg" alt='lens icons' width={10} height={10} className="pointer-events-none"/>
-      </button>
+
+       <EnableClickAnimation>
+        <CustomButton
+          customStyle="bg-Skobeloff gap-3 items-center flex text-white rounded-full px-5 py-2.5 ml-2 text-sm md:text-base font-medium focus:outline-none focus-visible:ring-2 hover:bg-darkslategray active:bg-darkslategray"
+        >
+            <>
+            Search
+            <Image src="right-arrow.svg" alt='lens icons' width={10} height={10} className="pointer-events-none"/>
+            </>
+        </CustomButton>
+       </EnableClickAnimation>
     </div>
   </div>
 </Form>
