552b0571865cf89f577e09d24da18b048fb0d96b
Hrishikesh <hrishikeshrajan3@gmail.com>
2024-11-15 10:50:52 +0530

    refactor: enable animation for auth button

--- app/components/AuthButton.tsx
+++ app/components/AuthButton.tsx
@@ -2,31 +2,38 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { useState } from 'react'
+import CustomButton from './theme/CustomButton'
+import EnableClickAnimation from './animation/EnableClickAnimation'
 
 export default function AuthButtons() {
   const [activeButton, setActiveButton] = useState('login')
 
   return (
     <div className='hidden lg:flex items-center space-x-4'>
-      <Button 
-        variant={'ghost'} 
-        className={`border-2 rounded-3xl text-Skobeloff border-Skobeloff nav-btn hover:bg-Skobeloff hover:text-white
-          ${activeButton === 'signup' ? 'bg-Skobeloff text-white' : 'hover:bg-Skobeloff hover:text-white'} hover:text-white`}
-        asChild
-        onClick={() => setActiveButton('signup')}
-      >
-        <Link href="#">Sign Up</Link>
-      </Button>
 
-      <Button 
-        variant={'ghost'} 
-        className={`border-2 rounded-3xl text-Skobeloff border-Skobeloff nav-btn hover:bg-Skobeloff hover:text-white
-          ${activeButton === 'login' ? 'bg-Skobeloff text-white' : 'hover:bg-Skobeloff hover:text-white'} hover:text-white`}
-        asChild
-        onClick={() => setActiveButton('login')}
-      >
-        <Link href="#" className='rounded-3xl'>Login</Link>
-      </Button>
+     <EnableClickAnimation>
+        <CustomButton
+              variant={'ghost'}
+              customStyle={`border-2 rounded-3xl text-Skobeloff border-Skobeloff nav-btn hover:bg-Skobeloff hover:text-white
+              ${activeButton === 'signup' ? 'bg-Skobeloff text-white' : 'hover:bg-Skobeloff hover:text-white'} hover:text-white`} 
+              asChild
+              onClick={() => setActiveButton('signup')}
+          >
+          <Link href="#">Sign Up</Link>
+          </CustomButton>
+     </EnableClickAnimation>
+     <EnableClickAnimation>
+        <CustomButton
+              variant={'ghost'}
+              customStyle={`border-2 rounded-3xl text-Skobeloff border-Skobeloff nav-btn hover:bg-Skobeloff hover:text-white
+              ${activeButton === 'login' ? 'bg-Skobeloff text-white' : 'hover:bg-Skobeloff hover:text-white'} hover:text-white`} 
+              asChild
+              onClick={() => setActiveButton('login')}
+          >
+          <Link href="#">Login</Link>
+          </CustomButton>
+     </EnableClickAnimation>
+
     </div>
   )
 }
